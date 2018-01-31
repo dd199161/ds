770b31dc3d72552c900158a56c11bc21796e445d
himnathan <1121380402@qq.com>
2018-01-31 19:52:44 +0800

    nathan

--- vuexCode/src/components/vuexcount.vue
+++ vuexCode/src/components/vuexcount.vue
@@ -1,30 +1,36 @@
 <template>
-	<div class="counta">
-		<p>{{ $store.state.count }}</p>
-		<button >{{ $store.state.count }}</button>
-		<button @click="increment">+1</button>
-		<button @click="decrement">-1</button>
-		<vuexa></vuexa>
-	</div>
+  <div class="counta">
+    <p>{{ $store.state.count }}</p>
+    <button>{{ $store.state.count }}</button>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+    <vuexa></vuexa>
+  </div>
 </template>
-
 <script>
-import { mapGetters, mapActions} from 'vuex'
-import vuexa from './vuexcounta'
-export default {
-  data() {
-    return {
-			counts: 0
-		}
-	},
-  methods: mapActions(['increment', 'decrement']),
-	computed: mapGetters([]),
-	components:{
-		vuexa
+  import { mapGetters, mapActions } from 'vuex'
+  import vuexa from './vuexcounta'
+  export default {
+    data() {
+      return {
+        counts: 0
+      }
+    },
+    methods:{
+    	...mapActions(['increment','decrement'])
+    }
+    computed:{
+		...mapGetters([])
 	}
-}
+    
+    /* 与上面的是一样的，如果不用再写其它的就可以直接简写 */
+//  methods: mapActions(['increment', 'decrement']),
+//  computed: mapGetters([]),
+    components: {
+      vuexa
+    }
+  }
 </script>
-
 <style>
 
 </style>
