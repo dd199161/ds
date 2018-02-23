c5b692a59361fb5c8b3eb43df4052f378b536dcd
himnathan <1121380402@qq.com>
2018-02-23 12:37:33 +0800

    Revert "fdsa"
    This reverts commit 8c11d7a3191478193843baff303c601a88760640.

--- alpha-caishi/middleware/docable.js
+++
@@ -1,9 +0,0 @@
-export default function({ app, store, error }) {
-  return app.$axios.$post(
-    '/api/doc_auth',
-    {
-      pw: store.state.docable
-    },
-    { baseURL: '/' }
-  )
-}
--- alpha-caishi/pages/home1.vue
+++
@@ -1,8 +0,0 @@
-<template src="../templates/home/index1.html"> </template>
-</template>
-
-<script>
-</script>
-
-<style src="../assets/c8css/index1.css">
-</style>
